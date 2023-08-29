7f305a228fb12b27cf2cbb6a3d95058da62b18a3
FilipeHaniel <filipe.h.martins@timwe.com>
2023-08-28 22:04:46 -0300

    finaly user repository

--- lib/src/repositories/user/user_repository.dart
+++ lib/src/repositories/user/user_repository.dart
@@ -1,6 +1,10 @@
 import 'package:dw_barbershop_app/src/core/exceptions/auth_exception.dart';
+import 'package:dw_barbershop_app/src/core/exceptions/repository_exception.dart';
 import 'package:dw_barbershop_app/src/core/funcional_programming/either.dart';
+import 'package:dw_barbershop_app/src/models/user_model.dart';
 
 abstract interface class UserRepository {
   Future<Either<AuthException, String>> login(String email, String password);
+
+  Future<Either<RepositoryException, UserModel>> me();
 }
--- lib/src/repositories/user/user_repository_impl.dart
+++ lib/src/repositories/user/user_repository_impl.dart
@@ -3,8 +3,10 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:dw_barbershop_app/src/core/exceptions/auth_exception.dart';
+import 'package:dw_barbershop_app/src/core/exceptions/repository_exception.dart';
 import 'package:dw_barbershop_app/src/core/funcional_programming/either.dart';
 import 'package:dw_barbershop_app/src/core/rest_client/rest_client.dart';
+import 'package:dw_barbershop_app/src/models/user_model.dart';
 import 'package:dw_barbershop_app/src/repositories/user/user_repository.dart';
 
 class UserRepositoryImpl implements UserRepository {
@@ -38,6 +40,23 @@ class UserRepositoryImpl implements UserRepository {
       log('Erro ao realizar login', error: e, stackTrace: s);
 
       return Failure(AuthError(message: 'Erro ao realizar login'));
+    }
+  }
+
+  @override
+  Future<Either<RepositoryException, UserModel>> me() async {
+    try {
+      final Response(:data) = await _restClient.auth.get('/me');
+
+      return Success(UserModel.fromMap(data));
+    } on DioException catch (e, s) {
+      log('Erro ao buscar usuário logado', error: e, stackTrace: s);
+
+      return Failure(
+          RepositoryException(message: 'Erro ao buscar usuário logado'));
+    } on ArgumentError catch (e, s) {
+      log('Invalid json', error: e, stackTrace: s);
+      return Failure(RepositoryException(message: e.message));
     }
   }
 }
